bbf53049d5095a21701a84ae056575c0e5798d38
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2024-07-31 22:35:46 +0545

    day 11: promises, async and await in JS

--- Day_11_PromisesAsyncAwait/promises_async_await.js
+++ Day_11_PromisesAsyncAwait/promises_async_await.js
@@ -2,12 +2,13 @@
 const promise1 = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		console.log("Async task 1");
-		resolve();
+		resolve({post:"Test",comment:"This is a test"});
 	}, 2000)
 });
 
 promise1
-	.then(() => {
+	.then((userPost) => {
+		console.log(userPost);
 		console.log("Promise consumed 1");
 	})
 
@@ -15,16 +16,17 @@ promise1
 const promise2 = new Promise((resolve, reject) => {
 	setTimeout(
 		() => {
-			let error = true;
+			let error = false;
 			if (!error) {
-				resolve();
+				resolve({title:"The Art of Living","author":"Sam"});
 			}
 			else {
 				reject();
 			}
 		}, 2000);
 });
-promise2.then(() => {
+promise2.then((book) => {
+	console.log(book);
 	console.log("Promise 2 consumed");
 })
 	.catch(() => {
@@ -55,27 +57,71 @@ promise3
 
 
 //Using Async/Await
-const promise4 = new Promise((resolve,reject)=>{
-	setTimeout(()=>{
+const promise4 = new Promise((resolve, reject) => {
+	setTimeout(() => {
 		let error = false;
-		if(!error){
-			resolve({userName:"Hitesh", email:"hitesh@example.com"});
+		if (!error) {
+			resolve({ userName: "Hitesh", email: "hitesh@example.com" });
 		}
-		else{
+		else {
 			reject("Error 4: Something went wrong");
 		}
-	},2000);
+	}, 2000);
 })
-async function getAsyncData(){
+async function getAsyncData() {
 	try {
 		const response = await promise4;
 		console.log(response);
 	} catch (error) {
 		console.log(error);
 	}
-	
-} 
+
+}
 getAsyncData();
 
 //Fetch Data from an API
-const data = fetch('https://jsonplaceholder.typicode.com/users')
+//Fetch execute in the whole program since it is a high priority task in priority queue ahead of any other operations
+fetch('https://jsonplaceholder.typicode.com/users')
+	.then((data) => {
+		return data.json();
+	})
+	.then((response) => {
+		console.log("Response using fetch and Promise:", response);
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+async function getAsynAwaitResult() {
+	const data = await fetch('https://jsonplaceholder.typicode.com/users');
+	// data.json() returns Promise
+	const response = await data.json();
+	console.log("Response using Fetch and async await:", response);
+}
+
+getAsynAwaitResult();
+
+console.log("Output using Promise.all:");
+//Promise.all takes an iterable of promises as input and returns a single promises, The returned promise fullfills when all of the input's promises fulfill 
+//and rejects when any of them is rejected
+Promise.all([promise1, promise2, promise3])
+	.then((values) => {
+		console.log("From promise all resolve:",values);
+	})
+	.catch((err)=>
+	{
+		console.log("From promise reject:",err);
+	});
+
+const promise6 = new Promise((resolve,reject)=>{
+	setTimeout(resolve,500,'test1');
+})
+const promise7 = new Promise((resolve,reject)=>{
+	setTimeout(resolve,100,'test2');
+})
+
+//Give 'test2' as output since timeout is less and occurs even before fetch() api if fetch api takes time more than 100 millisecond
+Promise.race([promise6, promise7])
+.then((value)=>{
+	console.log("Promise race result:",value);
+})
